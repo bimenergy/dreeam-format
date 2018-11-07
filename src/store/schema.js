1dde47eaaf5c59e052b4dd9631c67fd0a07fba7c
andreasrudena <andreas@rudena.se>
2018-11-07 15:24:17 +0100

    chore:remvoe log

--- src/store/schema.js
+++ src/store/schema.js
@@ -78,8 +78,6 @@ const initialState = {
   selectedEntityId: rootSchema.$id, // store an object from schema (or a complete schema)
 };
 
-console.log(mainSchemas);
-
 /**
  * Keeps the result from simulation to update different result views
  */
